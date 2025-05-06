ba4be50e7c01c54bf2f0adbb2abb7303ae754649
UrielHeredia <129795896+UrielHeredia@users.noreply.github.com>
2025-05-06 17:02:35 -0300

    19 intento

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
-
+const path = require('path'); 
 const app = express();
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
   });
-  
+   
 app.use(express.static('public'));
 
 app.use(express.json());
