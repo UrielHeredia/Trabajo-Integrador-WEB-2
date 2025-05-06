f3a27df7f4c5df52bad5dd42ed00fb724b8a937d
UrielHeredia <129795896+UrielHeredia@users.noreply.github.com>
2025-05-06 16:35:32 -0300

    s

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require('express');
 
 const app = express();
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'build')));
 
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+});
+
+app.get('/', (req, res) => {
+    res.send('Servidor funcionando correctamente');
+  });
 
 app.use(express.static('public'));
 
